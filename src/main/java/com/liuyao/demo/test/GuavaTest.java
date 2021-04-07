8ca51ab1770c4868f8d959d40a120054a794c2b7
刘耀 <zl_liuy@163.com>
2021-04-08 05:40:47 +0800

    note

--- src/main/java/com/liuyao/demo/test/GuavaTest.java
+++ src/main/java/com/liuyao/demo/test/GuavaTest.java
@@ -2,6 +2,8 @@ package com.liuyao.demo.test;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -14,18 +16,28 @@ public class GuavaTest {
         Cache<String, String> cache = CacheBuilder.newBuilder()
 //                .maximumSize(2)
                 .expireAfterWrite(2, TimeUnit.SECONDS)
+                .removalListener(new RemovalListener<String, String>() {
+                    @Override
+                    public void onRemoval(RemovalNotification<String, String> notification) {
+                        System.out.println("delete " + notification.getKey());
+                    }
+                })
                 .build();
 
         cache.put("a", "aa");
         cache.put("b", "bb");
         cache.put("c", "cc");
 
+
         System.out.println(cache.size());
         System.out.println(cache.getIfPresent("a"));
         System.out.println(cache.getIfPresent("b"));
         System.out.println(cache.getIfPresent("c"));
 
+        cache.invalidate("a");
+
         try {
+            System.out.println("sleep...");
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             e.printStackTrace();
